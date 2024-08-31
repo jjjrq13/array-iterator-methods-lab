03602778d3ebdbd6020c31fc1f497120a4f3290e
JonathanRQ <jonathan.ruizquintero@gmail.com>
2024-08-31 01:31:24 -0400

    3, 4 & 5 out the way

--- app.js
+++ app.js
@@ -12,43 +12,83 @@ const inventors = [
     { first: 'Ada', last: 'Lovelace', year: 1815, passed: 1852 },
     { first: 'Sarah E.', last: 'Goode', year: 1855, passed: 1905 },
     { first: 'Lise', last: 'Meitner', year: 1878, passed: 1968 },
-    { first: 'Hanna', last: 'Hammarström', year: 1829, passed: 1909 }
-  ];
+    { first: 'Hanna', last: 'Hammarström', year: 1829, passed: 1909 },
+];
 
-  const people = [
-    'Becker, Carl', 'Beckett, Samuel', 'Beddoes, Mick', 'Beecher, Henry',
-    'Beethoven, Ludwig', 'Begin, Menachem', 'Belloc, Hilaire', 'Bellow, Saul',
-    'Benchley, Robert', 'Benenson, Peter', 'Ben-Gurion, David',
-    'Benjamin, Walter', 'Benn, Tony', 'Bennington, Chester', 'Benson, Leana',
-    'Bent, Silas', 'Bentsen, Lloyd', 'Berger, Ric', 'Bergman, Ingmar',
-    'Berio, Luciano', 'Berle, Milton', 'Berlin, Irving', 'Berne, Eric',
-    'Bernhard, Sandra', 'Berra, Yogi', 'Berry, Halle', 'Berry, Wendell',
-    'Bethea, Erin', 'Bevan, Aneurin', 'Bevel, Ken', 'Biden, Joseph',
-    'Bierce, Ambrose', 'Biko, Steve', 'Billings, Josh', 'Biondo, Frank',
-    'Birrell, Augustine', 'Black, Elk', 'Blair, Robert', 'Blair, Tony',
-    'Blake, William'
-  ];
-  
-  const travelMethods = [
-    'car', 'car', 'truck', 'truck', 'bike', 'walk', 'car', 'van',
-    'bike', 'walk', 'car', 'van', 'car', 'truck'
-  ];
+const people = [
+    'Becker, Carl',
+    'Beckett, Samuel',
+    'Beddoes, Mick',
+    'Beecher, Henry',
+    'Beethoven, Ludwig',
+    'Begin, Menachem',
+    'Belloc, Hilaire',
+    'Bellow, Saul',
+    'Benchley, Robert',
+    'Benenson, Peter',
+    'Ben-Gurion, David',
+    'Benjamin, Walter',
+    'Benn, Tony',
+    'Bennington, Chester',
+    'Benson, Leana',
+    'Bent, Silas',
+    'Bentsen, Lloyd',
+    'Berger, Ric',
+    'Bergman, Ingmar',
+    'Berio, Luciano',
+    'Berle, Milton',
+    'Berlin, Irving',
+    'Berne, Eric',
+    'Bernhard, Sandra',
+    'Berra, Yogi',
+    'Berry, Halle',
+    'Berry, Wendell',
+    'Bethea, Erin',
+    'Bevan, Aneurin',
+    'Bevel, Ken',
+    'Biden, Joseph',
+    'Bierce, Ambrose',
+    'Biko, Steve',
+    'Billings, Josh',
+    'Biondo, Frank',
+    'Birrell, Augustine',
+    'Black, Elk',
+    'Blair, Robert',
+    'Blair, Tony',
+    'Blake, William',
+];
 
-  const devs = [
+const travelMethods = [
+    'car',
+    'car',
+    'truck',
+    'truck',
+    'bike',
+    'walk',
+    'car',
+    'van',
+    'bike',
+    'walk',
+    'car',
+    'van',
+    'car',
+    'truck',
+];
+
+const devs = [
     { name: 'Alex', year: 1988 },
     { name: 'Dani', year: 1986 },
     { name: 'Matt', year: 1970 },
-    { name: 'Wes', year: 2015 }
-  ];
-  
-  const comments = [
+    { name: 'Wes', year: 2015 },
+];
+
+const comments = [
     { text: 'Love this!', id: 523423 },
     { text: 'Super good', id: 823423 },
     { text: 'You are the best', id: 2039842 },
     { text: 'Ramen is my fav food ever', id: 123523 },
-    { text: 'Nice Nice Nice!', id: 542328 }
-  ];
-  
+    { text: 'Nice Nice Nice!', id: 542328 },
+];
 
 /*
 Exercise 1: Array.prototype.filter()
@@ -67,20 +107,16 @@ let veryOldInventors = [];
 
 // Complete the exercise in the space below:
 
-
 veryOldInventors = inventors.filter((inventors) => {
-  return inventors.year < 1600 && inventors.year > 1499;
-})
+    return inventors.year < 1600 && inventors.year > 1499;
+});
 
 // Check your work:
-console.log('Exercise 1 my result: ', veryOldInventors)
-console.log(
-  'Exercise 1 correct result: ',
-  [
+console.log('Exercise 1 my result: ', veryOldInventors);
+console.log('Exercise 1 correct result: ', [
     { first: 'Galileo', last: 'Galilei', year: 1564, passed: 1642 },
-    { first: 'Johannes', last: 'Kepler', year: 1571, passed: 1630 }
-  ]
-)
+    { first: 'Johannes', last: 'Kepler', year: 1571, passed: 1630 },
+]);
 
 /*
 Exercise 2: Array.prototype.map()
@@ -97,19 +133,18 @@ Hint: Return a new object literal from the callback that looks like:
       { first: "First Name", last: "Last Name" }
 */
 
-let inventorNames = []
+let inventorNames = [];
 
 // Complete the exercise in the space below:
 
-inventorNames = inventors.map((inventors) => ({first: inventors.first, last: inventors.last}));
-
-
+inventorNames = inventors.map((inventors) => ({
+    first: inventors.first,
+    last: inventors.last,
+}));
 
 // Check your work:
-console.log('Exercise 2 my result: ', inventorNames)
-console.log(
-  'Exercise 2 correct result: ',
-  [
+console.log('Exercise 2 my result: ', inventorNames);
+console.log('Exercise 2 correct result: ', [
     { first: 'Albert', last: 'Einstein' },
     { first: 'Isaac', last: 'Newton' },
     { first: 'Galileo', last: 'Galilei' },
@@ -121,7 +156,133 @@ console.log(
     { first: 'Ada', last: 'Lovelace' },
     { first: 'Sarah E.', last: 'Goode' },
     { first: 'Lise', last: 'Meitner' },
-    { first: 'Hanna', last: 'Hammarström' }
-  ]
-)
+    { first: 'Hanna', last: 'Hammarström' },
+]);
 
+/*
+Exercise 3: Array.prototype.sort()
+
+Sort the inventors by birth date in ascending order (from those born furthest in
+the past to those born most recently).
+*/
+
+let sortedByBirthYear = [];
+
+// Complete the exercise in the space below:
+
+sortedByBirthYear = inventors.sort((a, b) => a.year - b.year);
+
+// Check your work:
+console.log('Exercise 3 my result: ', sortedByBirthYear);
+console.log('Exercise 3 correct result: ', [
+    { first: 'Nicolaus', last: 'Copernicus', year: 1473, passed: 1543 },
+    { first: 'Galileo', last: 'Galilei', year: 1564, passed: 1642 },
+    { first: 'Johannes', last: 'Kepler', year: 1571, passed: 1630 },
+    { first: 'Isaac', last: 'Newton', year: 1643, passed: 1727 },
+    { first: 'Ada', last: 'Lovelace', year: 1815, passed: 1852 },
+    { first: 'Hanna', last: 'Hammarström', year: 1829, passed: 1909 },
+    { first: 'Sarah E.', last: 'Goode', year: 1855, passed: 1905 },
+    { first: 'Max', last: 'Planck', year: 1858, passed: 1947 },
+    { first: 'Marie', last: 'Curie', year: 1867, passed: 1934 },
+    { first: 'Lise', last: 'Meitner', year: 1878, passed: 1968 },
+    { first: 'Albert', last: 'Einstein', year: 1879, passed: 1955 },
+    { first: 'Katherine', last: 'Blodgett', year: 1898, passed: 1979 },
+]);
+
+/* 
+Exercise 4: Array.prototype.find()
+
+Use Array.prototype.find() to identify the inventor with the first name 'Ada' 
+from an array of inventor objects
+
+- You have an array of objects, each representing an inventor with various 
+  properties including their first name.
+- Utilize the Array.prototype.find() method to locate the object for the 
+  inventor with the first name 'Ada'.
+- Assign the found inventor object to the variable inventorNamedAda
+*/
+
+let inventorNamedAda = {};
+
+// Complete the exercise in the space below:
+
+inventorNamedAda = inventors.find((name) => {
+    return name.first === 'Ada';
+});
+
+// Check your work:
+console.log('Exercise 4 my result: ', inventorNamedAda);
+console.log('Exercise 4 correct result: ', {
+    first: 'Ada',
+    last: 'Lovelace',
+    year: 1815,
+    passed: 1852,
+});
+
+/*
+Exercise 5: Array.prototype.map()
+
+Use the Array.prototype.map() method to reformat each name in the 'people' 
+array. The goal is to convert names from "Last, First" format to "First Last" 
+format.
+
+Hint: Use the String.prototype.split() method to separate the first and last 
+      names. You can split the string using ', ' as the separator.
+      After splitting the names, rearrange them to the "First Last" format.
+*/
+
+let firstLast = [];
+
+// Complete the exercise in the space below:
+
+people.forEach((x) => {
+    firstLast.push(x.split(','));
+});
+
+firstLast = firstLast.map((element) => `${element[1]} ${element[0]}`);
+console.log(firstLast);
+
+// Check your work:
+console.log('Exercise 5 my result: ', firstLast);
+console.log('Exercise 5 correct result: ', [
+    'Carl Becker',
+    'Samuel Beckett',
+    'Mick Beddoes',
+    'Henry Beecher',
+    'Ludwig Beethoven',
+    'Menachem Begin',
+    'Hilaire Belloc',
+    'Saul Bellow',
+    'Robert Benchley',
+    'Peter Benenson',
+    'David Ben-Gurion',
+    'Walter Benjamin',
+    'Tony Benn',
+    'Chester Bennington',
+    'Leana Benson',
+    'Silas Bent',
+    'Lloyd Bentsen',
+    'Ric Berger',
+    'Ingmar Bergman',
+    'Luciano Berio',
+    'Milton Berle',
+    'Irving Berlin',
+    'Eric Berne',
+    'Sandra Bernhard',
+    'Yogi Berra',
+    'Halle Berry',
+    'Wendell Berry',
+    'Erin Bethea',
+    'Aneurin Bevan',
+    'Ken Bevel',
+    'Joseph Biden',
+    'Ambrose Bierce',
+    'Steve Biko',
+    'Josh Billings',
+    'Frank Biondo',
+    'Augustine Birrell',
+    'Elk Black',
+    'Robert Blair',
+    'Tony Blair',
+    'William Blake',
+]);
